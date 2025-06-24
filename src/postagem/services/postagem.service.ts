5054f7565360f54d9e0a9ee62a642dee99ad2462
santana-larissa <santanalarissa39@gmail.com>
2025-06-24 16:43:17 -0300

    ajeitei um código

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -28,7 +28,7 @@ export class PostagemService {
     return postagem;
   }
 
-  async findByTitulo(titulo: string): Promise<Postagem[]>{
+  async findAllByTitulo(titulo: string): Promise<Postagem[]>{
     return await this.postagemRepository.find({
       where:{
         titulo: ILike(`%${titulo}%`)
